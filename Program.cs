25baca8a3b24853e5c332ca8dfbc5410a6201396
Teolana09 <lanari.matteo@franchettisalviani.net>
2026-04-20 09:50:33 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EsFileN3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("numeri.txt"))
+            {
+                File.Create("numeri.txt");
+            }
+            List<int> numeri = new List<int>();
+            string[] arrayTemporaneo = File.ReadAllLines("numeri.txt");
+            int media;
+            int somma = 0;
+            for (int i = 0; i < arrayTemporaneo.Length; i++)
+            {
+                if (Convert.ToInt32(arrayTemporaneo[i] % 2 == 0))
+                {
+                    numeri.Add(Convert.ToInt32(arrayTemporaneo[i]));
+                    somma += numeri[i];
+                }
+                Console.WriteLine("[" + i + "]");
+            }
+            media = somma / numeri.Count;
+            Console.WriteLine(media);
+        }
+    }
+}
